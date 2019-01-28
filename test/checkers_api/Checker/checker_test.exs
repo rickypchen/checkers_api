069257b3eeda0e58dc9d95af670d1ec9190f5d32
Ricky Chen <richard.polin.chen@gmail.com>
2019-01-28 07:32:36 -0500

    Add more tests for checker context

--- test/checkers_api/Checker/checker_test.exs
+++ test/checkers_api/Checker/checker_test.exs
@@ -1,6 +1,6 @@
 defmodule CheckersApi.CheckerTest do
   use CheckersApi.DataCase
-  alias CheckersApi.Checker
+  alias CheckersApi.{Checker, Checker.Server}
 
   describe "list_servers/0" do
     test "should return all servers" do
@@ -18,6 +18,44 @@ defmodule CheckersApi.CheckerTest do
     test "should return nil if server not found" do
       id = UUID.uuid1()
       assert Checker.get_server(id) == nil
+    end
+  end
+
+  describe "create_server/1" do
+    @valid_attrs %{ name: "test", ip_address: "0.0.0.0" }
+    @invalid_attrs %{ name: nil, ip_address: "0.0.0.0" }
+
+    test "should create a server" do
+      assert {:ok, server} = Checker.create_server(@valid_attrs)
+      assert server.name == @valid_attrs.name
+    end
+
+    test "should not create a server with invalid attrs" do
+      assert {:error, changeset} = Checker.create_server(@invalid_attrs)
+    end
+  end
+
+  describe "update_server/1" do
+    @valid_update_attrs %{ name: "test2" }
+    @invalid_update_attrs %{ name: nil }
+
+    test "should update a server with valid update attrs" do
+      server = insert(:server, name: "test1")
+      assert {:ok, server} = Checker.update_server(server, @valid_update_attrs)
+      assert server.name == "test2"
+    end
+
+    test "should not update a server with invalid update attrs" do
+      server = insert(:server, name: "test1")
+      assert {:error, _changeset} = Checker.update_server(server, @invalid_update_attrs)
+    end
+  end
+
+  describe "delete_server/1" do
+    test "should delete a server" do
+      server = insert(:server)
+      assert {:ok, server} = Checker.delete_server(server)
+      assert Repo.get(Server, server.id) == nil
     end
   end
 end
